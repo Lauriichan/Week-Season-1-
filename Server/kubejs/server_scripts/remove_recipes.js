54770b2959c854ae21a9fbc72eaf1e40289a61d3
Lauriichan <lauriichan@playuniverse.org>
2026-03-22 13:36:45 +0100

    Update

--- Server/kubejs/server_scripts/remove_recipes.js
+++ Server/kubejs/server_scripts/remove_recipes.js
@@ -1,3 +1,10 @@
 ServerEvents.recipes(event => {
-    
+    event.remove({
+        mod: 'magistuarmory',
+        output: '#c:ingots/steel'
+    });
+    event.remove({
+        mod: 'magistuarmory',
+        output: '#c:nuggets/steel'
+    });
 });
